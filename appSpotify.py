e14188e8130d1606db28601d4da5429c60d6d427
cabobadilla <cbobadilla.dsoft@gmail.com>
2025-01-04 08:22:02 -0300

    Update appSpotify.py

--- appSpotify.py
+++ appSpotify.py
@@ -183,7 +183,7 @@ def main():
         st.markdown("<h2>🎶 Generar y Crear Lista de Reproducción</h2>", unsafe_allow_html=True)
         user_id = st.text_input("🎤 Introduce tu ID de usuario de Spotify", placeholder="Usuario de Spotify")
         mood = st.selectbox("😊 Selecciona tu estado de ánimo deseado", ["Concentración", "Trabajo", "Caminar", "Deporte", "Descanso"])
-        genres = st.multiselect("🎸 Selecciona los géneros musicales", ["Rock Pesado", "Rock 80 y 90s", "Rock","Pop", "Rock Progresivo", "Hip Hop", "Rap"])
+        genres = st.multiselect("🎸 Selecciona los géneros musicales", ["Rock Pesado", "Rock 80 y 90s", "Rock", "Pop", "Rock Progresivo", "Hip Hop", "Rap"])
 
         if st.button("🎵 Generar y Crear Lista 🎵"):
             if user_id and mood and genres:
